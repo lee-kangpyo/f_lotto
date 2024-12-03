00ac1efebdb96a49b3b2f978bcfc6b689ade7dea
그램 <darkzhon@hanmail.net>
2024-12-03 18:04:12 +0900

    로또 번호를 렌더링하는 위젯에 보너스 번호도 렌더링 하도록 추가

--- lib/widgets/GenerateLotto/LottoNumbers.dart
+++ lib/widgets/GenerateLotto/LottoNumbers.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:lotto/util/lotto.dart';
 import 'package:lotto/widgets/common/SingleLottoBall.dart';
 
 class LottoNumbers extends StatelessWidget {
-  const LottoNumbers({super.key, required this.lottoNumbers});
+  const LottoNumbers({super.key, required this.lottoNumbers, this.bonus});
   final List<int> lottoNumbers;
+  final int? bonus;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: lottoNumbers.map<Widget>((el) {
-        return SingleLottoBall(number: el, size: 35);
-      }).toList(),
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: lottoNumbers.map<Widget>((el) {
+            return SingleLottoBall(number: el, size: 35);
+          }).toList(),
+        ),
+        (bonus != null)?
+              Row(
+                children: [
+                  const Text("+"),
+                  SingleLottoBall(number: bonus!, size: 35),
+                ],
+              )
+            :
+              const SizedBox.shrink(),
+      ],
     );
   }
 }
